4f508c8f04d767f8240d3942dfa37ed0dcc50de6
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-03-09 20:13:56 +0100

    UI: Button

--- Notes/ContentView.swift
+++ Notes/ContentView.swift
@@ -24,17 +24,39 @@ struct ContentView: View {
             .background(Color.red)
             .frame(width: 200, height: 200)
         
+        
         Rectangle()
             .frame(width: 200, height: 200)
             .foregroundStyle(Color.yellow)
             .cornerRadius(100)
             .opacity(0.5)
-    
+        
+        /*
         Ellipse()
             .stroke(lineWidth: 5)
             .frame(width: 200, height: 400)
             .foregroundStyle(Color.red)
             .rotationEffect(Angle(degrees: 30))
+        */
+    
+        RoundedRectangle(cornerRadius: 25)
+            .stroke(style: .init(lineWidth: 15))
+            .foregroundStyle(.ultraThickMaterial)
+            .background(
+                LinearGradient(
+                    colors: [Color.blue, Color.purple],
+                    startPoint: .leading,
+                    endPoint: .trailing
+                )
+            )
+            .clipShape(RoundedRectangle(cornerRadius: 25))
+            .frame(height: 75)
+            .overlay {
+                Text("Press me")
+                    .foregroundStyle(.white)
+                    .font(.system(size: 20, weight: .semibold))
+            }
+         
         
     }
 }
