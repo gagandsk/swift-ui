129eba06b1ada1ff9fc5c4b11ac87a94207ffd4d
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-03-10 13:13:27 +0100

    UI: Toggle

--- Notes/Extras/TogglePractice.swift
+++ Notes/Extras/TogglePractice.swift
@@ -9,10 +9,20 @@ import SwiftUI
 
 struct TogglePractice: View {
     @State var age: Double = 18
+    @State var isAdult: Bool = false
     var body: some View {
         VStack{
             Text("Age: \(age.formatted(.number))")
             Slider(value: $age, in: 1...100, step: 1)
+            Toggle("Is Adult? ", isOn: $isAdult)
+        }
+        .padding()
+        .onChange(of: age) { newValue in
+            if age > 18 {
+                isAdult = true
+            } else {
+                isAdult = false
+            }
         }
     }
 }
