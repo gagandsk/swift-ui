1d7f1ae46591ce564c809b5f3102b36c6a0c463b
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-03-10 12:28:46 +0100

    UI: Custom Picker options

--- Notes/Extras/PickersPractice.swift
+++ Notes/Extras/PickersPractice.swift
@@ -10,18 +10,25 @@ import SwiftUI
 struct PickersPractice: View {
     @State var date: Date = Date()
     @State var category: Int = 0
+    @State var size: NcardType = .small
     var body: some View {
         /*
         DatePicker("date", selection: $date, displayedComponents: [.hourAndMinute])
             .datePickerStyle(.wheel)
             .padding()
         */
+        /*
         Picker("Categories", selection: $category) {
             Text("0").tag(0)
             Text("1").tag(1)
             Text("2").tag(2)
         }
         .pickerStyle(.segmented)
+        */
+        Picker("Tamaños", selection: $size){
+            Text("Pequeño").tag(NcardType.small)
+            Text("Mediano").tag(NcardType.medium)
+        }
     }
 }
 
