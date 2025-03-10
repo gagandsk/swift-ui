4dae569771e0ab388e902a85dba85003041422dd
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-03-10 14:10:45 +0100

    UI: Gestures Part 1

---
+++ Notes/Extras/GesturesPractice.swift
@@ -0,0 +1,23 @@
+//
+//  GesturesPractice.swift
+//  Notes
+//
+//  Created by Gagandeep Dass Kaur on 10/3/25.
+//
+
+import SwiftUI
+
+struct GesturesPractice: View {
+    @State var color: Color = .black
+    var body: some View {
+        Circle()
+            .foregroundStyle(color)
+            .onTapGesture {
+                color = .red
+            }
+    }
+}
+
+#Preview {
+    GesturesPractice()
+}
