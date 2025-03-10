66997ba7a3120a20e593af4fe48cbe9f665bf913
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-03-10 14:16:58 +0100

    UI: Gestures -> DragGesture

--- Notes/Extras/GesturesPractice.swift
+++ Notes/Extras/GesturesPractice.swift
@@ -9,15 +9,23 @@ import SwiftUI
 
 struct GesturesPractice: View {
     @State var color: Color = .black
+    @State var offset: CGSize = .zero
     var body: some View {
         Circle()
             .foregroundStyle(color)
+            .offset(offset)
             .onTapGesture(count: 2) {
                 color = .red
             }
             .onLongPressGesture(minimumDuration: 3){
                 color = .blue
             }
+            .gesture (
+                DragGesture()
+                    .onChanged { value in
+                        offset = value.translation
+                    }
+            )
     }
 }
 
