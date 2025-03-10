295543d5d4ff27affbd86160de51981a534a06d2
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-03-10 12:16:47 +0100

    UI: DatePicker simple

---
+++ Notes/Extras/PickersPractice.swift
@@ -0,0 +1,20 @@
+//
+//  PickersPractice.swift
+//  Notes
+//
+//  Created by Gagandeep Dass Kaur on 10/3/25.
+//
+
+import SwiftUI
+
+struct PickersPractice: View {
+    @State var date: Date = Date()
+    var body: some View {
+        DatePicker("date", selection: $date)
+            .padding()
+    }
+}
+
+#Preview {
+    PickersPractice()
+}
