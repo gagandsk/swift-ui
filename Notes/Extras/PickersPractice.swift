8035f81b8b3b0dd4c5a426f9a5217200fdd6afa1
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-03-10 12:25:19 +0100

    UI: Categories Picker example

--- Notes/Extras/PickersPractice.swift
+++ Notes/Extras/PickersPractice.swift
@@ -9,10 +9,19 @@ import SwiftUI
 
 struct PickersPractice: View {
     @State var date: Date = Date()
+    @State var category: Int = 0
     var body: some View {
+        /*
         DatePicker("date", selection: $date, displayedComponents: [.hourAndMinute])
             .datePickerStyle(.wheel)
             .padding()
+        */
+        Picker("Categories", selection: $category) {
+            Text("0").tag(0)
+            Text("1").tag(1)
+            Text("2").tag(2)
+        }
+        .pickerStyle(.segmented)
     }
 }
 
