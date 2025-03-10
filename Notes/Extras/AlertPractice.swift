a219daa6821a0e0c0e1c39d43c90c2488b76deb7
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-03-10 13:45:34 +0100

    UI: Alerts

---
+++ Notes/Extras/AlertPractice.swift
@@ -0,0 +1,26 @@
+//
+//  AlertPractice.swift
+//  Notes
+//
+//  Created by Gagandeep Dass Kaur on 10/3/25.
+//
+
+import SwiftUI
+
+struct AlertPractice: View {
+    @State var showAlert: Bool = false
+    var body: some View {
+        VStack{
+            Button("Show Alert") {
+                showAlert = true
+            }
+        }
+        .alert("Este es un mensaje de alerta", isPresented: $showAlert) {
+            
+        }
+    }
+}
+
+#Preview {
+    AlertPractice()
+}
