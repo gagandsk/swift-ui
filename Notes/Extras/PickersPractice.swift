62ea096ae1bda11f1fb01a9c34b292a0a35c4c61
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-03-10 12:18:01 +0100

    UI: DatePicker -> hourAndMinute

--- Notes/Extras/PickersPractice.swift
+++ Notes/Extras/PickersPractice.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct PickersPractice: View {
     @State var date: Date = Date()
     var body: some View {
-        DatePicker("date", selection: $date, displayedComponents: [.date])
+        DatePicker("date", selection: $date, displayedComponents: [.hourAndMinute])
             .padding()
     }
 }
